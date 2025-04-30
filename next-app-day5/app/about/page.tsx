43bb094a478c893294f7995ae6f297a61966bec1
izzy215 <113905712+izzy215@users.noreply.github.com>
2025-04-30 13:58:01 +0900

    day5 start

---
+++ next-app-day5/app/about/page.tsx
@@ -0,0 +1,9 @@
+import MyTitle from '../components/MyTitle'
+export default function AboutPage() {
+  return(
+    <div>
+      <MyTitle />
+      <p>이건 About 페이지입니다!</p>
+    </div>
+  );
+  }
---
+++ next-app-day5/app/components/MyTitle.tsx
@@ -0,0 +1,4 @@
+// app/components/MyTitle.tsx
+export default function MyTitle() {
+    return <h1 style={{ color: 'hotpink' }}>🌈 컴포넌트 분리 성공!</h1>;
+  }
